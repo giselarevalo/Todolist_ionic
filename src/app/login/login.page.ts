baac0354ea0b6981c8cfadda6472a0f2fbba0032
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-11-20 15:13:27 -0300

    Login validations

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,35 @@ import { AuthService } from '../services/auth.service';
 export class LoginPage {
   email: string = '';
   password: string = '';
+  submitted: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router, private toastController: ToastController) {}
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000, // Duración del toast en milisegundos
+      position: 'top', // Puedes ajustar la posición del toast
+    });
+    toast.present();
+  }
 
   login() {
+
+    this.submitted = true;
+
+    if (!this.email || !this.password) {
+      this.presentToast('Por favor, ingresa un correo electrónico o una contraseña válidos.');
+      return;
+    }
+
+
     const isAuthenticated = this.authService.login(this.email, this.password);
 
     if (isAuthenticated) {
       this.router.navigate(['/task-list']); 
     } else {
-      console.error('Error de inicio de sesión');
+      this.presentToast('Error de inicio de sesión');
       
     }
   }
